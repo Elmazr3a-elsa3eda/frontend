c7dc89fce8e4d1259fc2c212c9e83ec770be1ed6
unknown <abdohani362@gmail.com>
2024-02-14 00:36:39 +0200

    added color pallete and favico

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'green':'#9EFFA9',
+        'darckblue': '#9EFFA9',
+        'darkerblue':'#333146',
+        'black': '#29252C'
+      },
+      fontFamily:{
+        'protest': ["Protest Revolution", 'sans-serif'],
+        'honk': ["Honk", 'system-ui'],
+        'poppins': ["Poppins", 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
